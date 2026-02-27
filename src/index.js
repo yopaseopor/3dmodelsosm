1850c1cadf128dc2dcff37c8d46107b09462bf30
yopaseopor <yopaseopor@gmail.com>
2026-02-27 23:29:30 +0100

    update urls

--- src/index.js
+++ src/index.js
@@ -491,7 +491,7 @@ $(function () {
 									const modelConfig = window.models ? window.models.getModelConfig(modelFilename) : null;
 
 									// Set the model property for ol-cesium to use - use Cesium Model options object
-									const modelUrl = new URL(`./src/assets/models/${modelFilename}`, window.location.href).href;
+									const modelUrl = `./src/assets/models/${modelFilename}`;
 									const modelOptions = {
 										uri: modelUrl,
 										scale: modelConfig ? modelConfig.scale : 1.0,
